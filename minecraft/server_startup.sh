119ec8a481c985fbd1d4c708c65f512d10e81468
Huynh Tien <huynhqtienvtag@gmail.com>
2021-04-08 09:39:06 +0700

    Update server_startup.sh

--- minecraft/server_startup.sh
+++ minecraft/server_startup.sh
@@ -24,7 +24,7 @@ PATCH=true
 # The patcher file name
 PATCHER=patcher.jar
 # The patcher default download link
-PATCHER_DEFAULT_LINK="https://github.com/KibbleLands/KibblePatcher/releases/download/1.6.2/KibblePatcher-1.6.2.jar"
+PATCHER_DEFAULT_LINK="https://github.com/KibbleLands/KibblePatcher/releases/download/1.6.3/KibblePatcher-1.6.3.jar"
 # The patcher link file
 PATCHER_LINK_FILE="./patcher"
 # The patched jar name
